b0ccbc990105ec7e40a5b76cd3a02d88cb0bcb4e
HaooLee <907615065@qq.com>
2021-10-29 17:54:22 +0800

    fix:移动端文章标题溢出处理

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default function Index() {
                 style={{width: '100%', height: '100vh', position: 'absolute', zIndex: '0'}}
                 config={{
                     // radius: 30,
+                    mobileRadius:18
                     // backgroundColor: '#fafafa',
                     // flagScale:1,
                     // flagLat:39.56,
